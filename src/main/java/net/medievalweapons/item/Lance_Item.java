e448df0f0907af266ccf156ff0bf8e05a7bc068e
Globox1997 <mitschke11@web.de>
2020-11-29 16:25:02 +0100

    added rider bonus

--- src/main/java/net/medievalweapons/item/Lance_Item.java
+++ src/main/java/net/medievalweapons/item/Lance_Item.java
@@ -1,5 +1,6 @@
 package net.medievalweapons.item;
 
+import java.util.UUID;
 import java.util.function.Supplier;
 
 import com.google.common.collect.ImmutableMultimap;
@@ -7,13 +8,18 @@ import com.google.common.collect.Multimap;
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 
 import net.medievalweapons.entity.Lance_Entity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.world.World;
 
 public class Lance_Item extends SwordItem {
     private final ToolMaterial material;
@@ -21,6 +27,8 @@ public class Lance_Item extends SwordItem {
     public final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
     private final Supplier<EntityType<Lance_Entity>> typeSupplier;
     private EntityType<Lance_Entity> cachedType = null;
+    private static final UUID ATTACK_BONUS_MODIFIER_ID = UUID.fromString("fbd4e4e4-62f7-4108-9be3-eb6781231298");
+    private static final EntityAttributeModifier ATTACK_BONUS_MODIFIER;
 
     public Lance_Item(ToolMaterial material, int attackDamage, float attackSpeed,
             Supplier<EntityType<Lance_Entity>> typeSupplier, Settings settings) {
@@ -54,6 +62,30 @@ public class Lance_Item extends SwordItem {
     @Override
     public ToolMaterial getMaterial() {
         return this.material;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
+            if (player.hasVehicle() && !player.getAttributes()
+                    .hasModifierForAttribute(EntityAttributes.GENERIC_ATTACK_DAMAGE, ATTACK_BONUS_MODIFIER_ID)
+                    && selected) {
+                EntityAttributeInstance entityAttributeInstance = player
+                        .getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+                entityAttributeInstance.removeModifier(ATTACK_BONUS_MODIFIER);
+                entityAttributeInstance.addTemporaryModifier(ATTACK_BONUS_MODIFIER);
+            } else if (player.getAttributes().hasModifierForAttribute(EntityAttributes.GENERIC_ATTACK_DAMAGE,
+                    ATTACK_BONUS_MODIFIER_ID)) {
+                player.getAttributeInstance(EntityAttributes.GENERIC_ATTACK_DAMAGE)
+                        .removeModifier(ATTACK_BONUS_MODIFIER_ID);
+            }
+        }
+    }
+
+    static {
+        ATTACK_BONUS_MODIFIER = new EntityAttributeModifier(ATTACK_BONUS_MODIFIER_ID, "Sneaking attack bonus", 1.5D,
+                EntityAttributeModifier.Operation.ADDITION);
     }
 
 }
