89f8d3c5fabe5d9fbe99058df01af8b6bffd27d1
Globox1997 <mitschke11@web.de>
2021-04-07 22:29:59 +0200

    added parrying sounds

--- src/main/java/net/medievalweapons/init/SoundInit.java
+++ src/main/java/net/medievalweapons/init/SoundInit.java
@@ -8,12 +8,18 @@ public class SoundInit {
 
     public static final Identifier MAGIC_HEAL_AURA = new Identifier("medievalweapons:magic_heal_aura");
     public static final Identifier MAGIC_SHOT = new Identifier("medievalweapons:magic_shot");
+    public static final Identifier PARRYING = new Identifier("medievalweapons:parrying");
+    public static final Identifier SWORD_PARRYING = new Identifier("medievalweapons:sword_parrying");
+    public static SoundEvent SWORD_PARRYING_EVENT = new SoundEvent(MAGIC_HEAL_AURA);
     public static SoundEvent MAGIC_HEAL_AURA_EVENT = new SoundEvent(MAGIC_HEAL_AURA);
     public static SoundEvent MAGIC_SHOT_EVENT = new SoundEvent(MAGIC_SHOT);
+    public static SoundEvent PARRYING_EVENT = new SoundEvent(PARRYING);
 
     public static void init() {
         Registry.register(Registry.SOUND_EVENT, SoundInit.MAGIC_HEAL_AURA, MAGIC_HEAL_AURA_EVENT);
         Registry.register(Registry.SOUND_EVENT, SoundInit.MAGIC_SHOT, MAGIC_SHOT_EVENT);
+        Registry.register(Registry.SOUND_EVENT, SoundInit.PARRYING, PARRYING_EVENT);
+        Registry.register(Registry.SOUND_EVENT, SoundInit.SWORD_PARRYING, SWORD_PARRYING_EVENT);
     }
 
 }
