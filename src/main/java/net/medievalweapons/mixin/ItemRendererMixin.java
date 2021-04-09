ba51a5ef89eabf818662974cfdecbcc8971b0c25
Globox1997 <mitschke11@web.de>
2021-04-09 11:23:23 +0200

    added double handed blocking visual

--- src/main/java/net/medievalweapons/mixin/ItemRendererMixin.java
+++ src/main/java/net/medievalweapons/mixin/ItemRendererMixin.java
@@ -4,6 +4,7 @@ import net.medievalweapons.item.Big_Axe_Item;
 import net.medievalweapons.item.Healing_Staff_Item;
 import net.medievalweapons.item.Javelin_Item;
 import net.medievalweapons.item.Lance_Item;
+import net.medievalweapons.item.Long_Sword_Item;
 import net.medievalweapons.item.Mace_Item;
 import net.medievalweapons.item.Thalleous_Sword_Item;
 import net.medievalweapons.item.renderer.Big_Axe_Item_Renderer;
@@ -12,12 +13,12 @@ import net.medievalweapons.item.renderer.Javelin_Item_Renderer;
 import net.medievalweapons.item.renderer.Lance_Item_Renderer;
 import net.medievalweapons.item.renderer.Mace_Item_Renderer;
 import net.medievalweapons.item.renderer.Thalleous_Sword_Item_Renderer;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -25,29 +26,41 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
 
-  @Inject(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;II)V", at = @At("HEAD"), cancellable = true)
-  public void renderItemMixin(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode,
-      boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light,
-      int overlay, CallbackInfo info) {
-    BakedModel model = MinecraftClient.getInstance().getItemRenderer().getHeldItemModel(stack, world, entity);
-    if ((stack.getItem() instanceof Javelin_Item && Javelin_Item_Renderer.INSTANCE.render(entity, stack, renderMode,
-        leftHanded, matrices, vertexConsumers, light, overlay, model))
-        || (stack.getItem() instanceof Big_Axe_Item && Big_Axe_Item_Renderer.INSTANCE.render(entity, stack, renderMode,
-            leftHanded, matrices, vertexConsumers, light, overlay, model))
-        || (stack.getItem() instanceof Lance_Item && Lance_Item_Renderer.INSTANCE.render(entity, stack, renderMode,
-            leftHanded, matrices, vertexConsumers, light, overlay, model))
-        || (stack.getItem() instanceof Healing_Staff_Item && Healing_Staff_Item_Renderer.INSTANCE.render(entity, stack,
-            renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model))
-        || (stack.getItem() instanceof Thalleous_Sword_Item && Thalleous_Sword_Item_Renderer.INSTANCE.render(entity,
-            stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model))
-        || (stack.getItem() instanceof Mace_Item && Mace_Item_Renderer.INSTANCE.render(entity, stack, renderMode,
-            leftHanded, matrices, vertexConsumers, light, overlay, model))) {
-      info.cancel();
+    @Inject(method = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void renderItemMixin(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode,
+            boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light,
+            int overlay, CallbackInfo info, BakedModel bakedModel) {
+        if ((stack.getItem() instanceof Javelin_Item && Javelin_Item_Renderer.INSTANCE.render(entity, stack, renderMode,
+                leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))
+                || (stack.getItem() instanceof Big_Axe_Item && Big_Axe_Item_Renderer.INSTANCE.render(entity, stack,
+                        renderMode, leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))
+                || (stack.getItem() instanceof Lance_Item && Lance_Item_Renderer.INSTANCE.render(entity, stack,
+                        renderMode, leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))
+                || (stack.getItem() instanceof Healing_Staff_Item && Healing_Staff_Item_Renderer.INSTANCE.render(entity,
+                        stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))
+                || (stack.getItem() instanceof Thalleous_Sword_Item && Thalleous_Sword_Item_Renderer.INSTANCE.render(
+                        entity, stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))
+                || (stack.getItem() instanceof Mace_Item && Mace_Item_Renderer.INSTANCE.render(entity, stack,
+                        renderMode, leftHanded, matrices, vertexConsumers, light, overlay, bakedModel))) {
+            info.cancel();
+        }
     }
-  }
+
+    @Inject(method = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V"))
+    public void renderItemMixinTwo(LivingEntity entity, ItemStack stack, ModelTransformation.Mode renderMode,
+            boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light,
+            int overlay, CallbackInfo info) {
+        if (entity != null
+                && (renderMode == ModelTransformation.Mode.FIRST_PERSON_LEFT_HAND
+                        || renderMode == ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND)
+                && stack.getItem() instanceof Long_Sword_Item && entity.isBlocking()) {
+            matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(20F));
+        }
+    }
 
 }
