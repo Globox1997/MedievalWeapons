274033423416f35f8efc0c89f5f964ecd9921260
Globox1997 <mitschke11@web.de>
2024-09-24 13:15:06 +0200

    fixed blocking rendering

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 ### Added:
 - 
 ### Fixed:
-- 
+- Fixed blocking animation
 ### Changed:
 - Removed dependency on ARRP/BRRP
 - Applies when non compat mods are loaded
--- src/main/java/net/medievalweapons/mixin/client/BipedEntityModelMixin.java
+++ src/main/java/net/medievalweapons/mixin/client/BipedEntityModelMixin.java
@@ -1,6 +1,8 @@
 package net.medievalweapons.mixin.client;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -24,61 +26,86 @@ import net.minecraft.util.math.MathHelper;
 @Mixin(BipedEntityModel.class)
 public abstract class BipedEntityModelMixin<T extends LivingEntity> extends AnimalModel<T> implements ModelWithArms, ModelWithHead {
     @Shadow
+    @Mutable
+    @Final
     public ModelPart rightArm;
     @Shadow
+    @Mutable
+    @Final
     public ModelPart leftArm;
     @Shadow
+    @Mutable
+    @Final
     public ModelPart head;
 
     @Inject(method = "setAngles", at = @At(value = "TAIL"))
     private void setAnglesMixin(T livingEntity, float f, float g, float h, float i, float j, CallbackInfo info) {
-        if (!CompatInit.isBetterCombatLoaded && livingEntity.getOffHandStack().isEmpty() && !livingEntity.isSwimming() && !livingEntity.hasVehicle()
-                && livingEntity.getMainHandStack().getItem() != null) {
+        if (livingEntity.getOffHandStack().isEmpty() && !livingEntity.isSwimming() && !livingEntity.hasVehicle()
+                && livingEntity.getMainHandStack() != null) {
             if (livingEntity.getMainHandStack().isIn(TagInit.DOUBLE_HANDED_ITEMS) || livingEntity.getMainHandStack().getItem() instanceof LongSwordItem) {
-                this.rightArm.pitch = -0.8727F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
-                this.rightArm.yaw = -0.5672F;
-                this.rightArm.roll = 0.0F;
-                this.leftArm.pitch = -1.0472F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
-                this.leftArm.yaw = 0.829F;
-                this.leftArm.roll = -0.0436F;
-                if (this.handSwingProgress > 0) {
-                    float gx = 1.0F - this.handSwingProgress;
-                    float hx = MathHelper.sin(gx * 3.1415927F);
-                    float kx = this.head.pitch;
-                    if (kx < 0) {
-                        kx = 0.25F;
+                if (!CompatInit.isBetterCombatLoaded) {
+                    this.rightArm.pitch = -0.8727F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
+                    this.rightArm.yaw = -0.5672F;
+                    this.rightArm.roll = 0.0F;
+                    this.leftArm.pitch = -1.0472F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
+                    this.leftArm.yaw = 0.829F;
+                    this.leftArm.roll = -0.0436F;
+                    if (this.handSwingProgress > 0) {
+                        float gx = 1.0F - this.handSwingProgress;
+                        float hx = MathHelper.sin(gx * 3.1415927F);
+                        float kx = this.head.pitch;
+                        if (kx < 0) {
+                            kx = 0.25F;
+                        }
+                        float ix = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -((kx) - 0.7F) * 0.75F * 0.6F;
+                        this.rightArm.pitch = (float) ((double) this.rightArm.pitch - ((double) hx * 1.2D + (double) ix));
+                        this.leftArm.pitch = (float) ((double) this.leftArm.pitch - ((double) hx * 1.2D + (double) ix) * 1.2D) * 0.75F;
                     }
-                    float ix = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -((kx) - 0.7F) * 0.75F * 0.6F;
-                    this.rightArm.pitch = (float) ((double) this.rightArm.pitch - ((double) hx * 1.2D + (double) ix));
-                    this.leftArm.pitch = (float) ((double) this.leftArm.pitch - ((double) hx * 1.2D + (double) ix) * 1.2D) * 0.75F;
                 }
 
                 if (livingEntity.isBlocking()) {
-                    this.rightArm.pitch = -1.25F;
-                    this.leftArm.pitch = -1.17F;
-                    this.rightArm.roll = 0.7F;
+                    if (!CompatInit.isBetterCombatLoaded) {
+                        this.rightArm.pitch = -1.25F;
+                        this.leftArm.pitch = -1.17F;
+                        this.rightArm.roll = 0.7F;
+                    } else {
+                        this.rightArm.pitch = -1.25F;
+                        this.rightArm.yaw = -0.8f;
+                        this.leftArm.pitch = -1.25F;
+                        this.leftArm.yaw = 0.8f;
+                    }
                 }
             } else if (livingEntity.getMainHandStack().isIn(TagInit.ACROSS_DOUBLE_HANDED_ITEMS) || livingEntity.getMainHandStack().getItem() instanceof BigAxeItem) {
-                this.rightArm.pitch = -0.5236F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
-                this.rightArm.yaw = 0.0F;
-                this.rightArm.roll = 0.0F;
-                this.leftArm.pitch = -1.2217F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
-                this.leftArm.yaw = 0.0F;
-                this.leftArm.roll = 0.0F;
-                if (this.handSwingProgress > 0) {
-                    float gx = 1.0F - this.handSwingProgress;
-                    float hx = MathHelper.sin(gx * 3.1415927F);
-                    float kx = this.head.pitch;
-                    if (kx < 0) {
-                        kx = 0.25F;
+                if (!CompatInit.isBetterCombatLoaded) {
+                    this.rightArm.pitch = -0.5236F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
+                    this.rightArm.yaw = 0.0F;
+                    this.rightArm.roll = 0.0F;
+                    this.leftArm.pitch = -1.2217F + (MathHelper.cos(f * 0.6662F) * 2.0F * g * 0.5F / 15);
+                    this.leftArm.yaw = 0.0F;
+                    this.leftArm.roll = 0.0F;
+                    if (this.handSwingProgress > 0) {
+                        float gx = 1.0F - this.handSwingProgress;
+                        float hx = MathHelper.sin(gx * 3.1415927F);
+                        float kx = this.head.pitch;
+                        if (kx < 0) {
+                            kx = 0.25F;
+                        }
+                        float ix = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -((kx) - 0.7F) * 0.75F * 0.6F;
+                        this.rightArm.pitch = (float) ((double) this.rightArm.pitch - ((double) hx * 1.2D + (double) ix));
+                        this.leftArm.pitch = (float) ((double) this.leftArm.pitch - ((double) hx * 1.2D + (double) ix) * 1.2D) * 0.75F * 1.2F;
                     }
-                    float ix = MathHelper.sin(this.handSwingProgress * 3.1415927F) * -((kx) - 0.7F) * 0.75F * 0.6F;
-                    this.rightArm.pitch = (float) ((double) this.rightArm.pitch - ((double) hx * 1.2D + (double) ix));
-                    this.leftArm.pitch = (float) ((double) this.leftArm.pitch - ((double) hx * 1.2D + (double) ix) * 1.2D) * 0.75F * 1.2F;
                 }
                 if (livingEntity.isBlocking()) {
-                    this.rightArm.pitch = -1.0234747F;
-                    this.leftArm.pitch = -1.6393949F;
+                    if (!CompatInit.isBetterCombatLoaded) {
+                        this.rightArm.pitch = -1.0234747F;
+                        this.leftArm.pitch = -1.6393949F;
+                    }else{
+                        this.rightArm.pitch = -1.0234747F;
+                        this.leftArm.pitch = -1.3393949F;
+                        this.rightArm.yaw = -0.6f;
+                        this.leftArm.yaw = 0.15f;
+                        this.rightArm.roll = 1.0f;
+                    }
                 }
             }
         }
